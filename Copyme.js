84e3f8c65f746a39d47cd55d327907a94dfa0db2
dagzadag <37269752+dagzadag@users.noreply.github.com>
2018-10-24 14:20:31 +0200

    Create Copyme.js

---
+++ Copyme.js
@@ -0,0 +1,25 @@
+
+
+
+function generate() {
+  	var i ;
+           	var arra = [3,6,9,12,13,18,24,30];
+           	var inpu = '<input class="form-control copyMe" type="text"  value="'
+           	for (i = 0; i < 8; i++) {
+           		var k = $("#Url").val();
+	            $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + arra[i],
+	                function (data) {
+	                    if (data.Error == "Success") {
+	                        document.getElementById('DOSSIERForm').innerHTML += inpu + data.TotalRembourser.toFixed(2) +'">';
+	                    }
+	                    else { }
+
+	                }
+	            );
+			} 
+}
+function copyMe(){
+  var copyText =document.getElementsByClassName("copyMe")[0];
+  copyText.select();
+  document.execCommand("copy");
+}
