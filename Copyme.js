9287649a1f7768b5cfe6a0ee0b992fe3d620a3be
dagzadag <37269752+dagzadag@users.noreply.github.com>
2018-10-24 14:42:51 +0200

    Update Copyme.js

--- Copyme.js
+++ Copyme.js
@@ -4,7 +4,7 @@
 function generate() {
   	var i ;
            	var arra = [3,6,9,12,13,18,24,30];
-           	var inpu = '<input onclick ='copyMe()' class="form-control copyMe" type="text"  value="'
+           	var inpu = '<input onclick ="copyMe()" class="form-control copyMe" type="text"  value="'
            	for (i = 0; i < 8; i++) {
            		var k = $("#Url").val();
 	            $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + arra[i],
