1daf986340e33d2d2134fb189cb6be4346732468
dagzadag <37269752+dagzadag@users.noreply.github.com>
2018-10-26 20:38:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,11 @@ function screenShot(){
  	console.log("started");
 	var element = document.getElementById("captcha");
 	html2canvas(element).then(function(canvas) {
-   	// Export canvas as a blob 
-  	canvas.toBlob(function(blob) {
-        // Generate file download
-        window.saveAs(blob, "screenshot.png");
-    });
-});
+   	 // Export the canvas to its data URI representation
+   	 var base64image = canvas.toDataURL("image/png");
+    	// Open the image in a new window
+   	 window.open(base64image , "_blank");
+	});
 }
 function changeValue(){
   var str = document.getElementsByClassName('visualCaptcha-explanation')[0].innerHTML;
