ab1529b998cebd8371030cd4ae16f25fecd8aa15
dagzadag <37269752+dagzadag@users.noreply.github.com>
2018-11-12 22:46:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function timeStamp(){
  var result = issue[Object.keys(issue)[1]] ;
  console.log(issue.value);
- var count = '<p  id="waitSec">'+result+'</p>';
+ var count = '<p  id="timeW">'+result+'</p>';
  var docCount = document.getElementById("tips").innerHTML;
  document.getElementById("tips").innerHTML = docCount + count;
 }
