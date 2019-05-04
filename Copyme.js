5d54a00cc4ac34a193a448d9f6eee99713024e03
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-05-04 20:50:30 +0200

    Update Copyme.js

--- Copyme.js
+++ Copyme.js
@@ -3,7 +3,8 @@
 
 function generate() {
 	var k = $("#Url").val();
-	var ref = document.getElementsByClassName('dxgv')[1].innerHTML
+	var ref = document.getElementsByClassName('dxgv')[1].innerHTML;
+	console.log(ref);
 	// Get a reference to the database service
 	var database = firebase.database();
     	var one = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 3);
@@ -11,11 +12,11 @@ function generate() {
    	var three = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 18);
     	var four = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 24);
     	var five = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 30);
-	writeUserData( ref, one,one,tow,tow,tow,three,four,five)
+	writeUserData(  one,one,tow,tow,tow,three,four,five)
 }
 
-function writeUserData( ref, price3,price6,price9,price12,price13,price18,price24,price30) {
-  firebase.database().ref(ref+'/').set({
+function writeUserData( a, price3,price6,price9,price12,price13,price18,price24,price30) {
+  firebase.database().ref("'"+a+"'"+'/').set({
     price3: price3,
     price6: price6,
     price9:price9,
