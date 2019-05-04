0392dbb00c96d5a8a5874d2573111734b9fafbc1
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-05-04 20:46:23 +0200

    trying

--- Copyme.js
+++ Copyme.js
@@ -3,43 +3,26 @@
 
 function generate() {
 	var k = $("#Url").val();
-    var one = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 3);
-    var tow = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 9);
-    var three = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 18);
-    var four = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 24);
-    var five = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 30);
+	var ref = document.getElementsByClassName('dxgv')[1].innerHTML
+	// Get a reference to the database service
+	var database = firebase.database();
+    	var one = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 3);
+    	var tow = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 9);
+   	var three = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 18);
+    	var four = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 24);
+    	var five = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 30);
+	writeUserData( ref, one,one,tow,tow,tow,three,four,five)
+}
 
-    setTimeout(function(){
-    var inpu = '<div class="col-lg-3"><input  class="form-control copyMe" type="text"  value="';
-	document.getElementById('DOSSIERForm').innerHTML += inpu + one.responseJSON.TotalRembourser +'" id="one" onclick ="copyone()"></div>';
-	document.getElementById('DOSSIERForm').innerHTML += inpu + tow.responseJSON.TotalRembourser +'"id="tow" onclick ="copytow()"></div>';
-	document.getElementById('DOSSIERForm').innerHTML += inpu + three.responseJSON.TotalRembourser +'"id="three" onclick ="copythree()"></div>';
-	document.getElementById('DOSSIERForm').innerHTML += inpu + four.responseJSON.TotalRembourser +'"id="four" onclick ="copyfour()"></div>';
-	document.getElementById('DOSSIERForm').innerHTML += inpu + five.responseJSON.TotalRembourser +'"id="five" onclick ="copyfive()"></div>';    
-    },4000)
-}
-function copyone(){
-  var copone =document.getElementById("one");
-  copone.select();
-  document.execCommand("copy");
-}
-function copytow(){
-  var coptow =document.getElementById("tow");
-  coptow.select();
-  document.execCommand("copy");
-}
-function copythree(){
-  var copthree =document.getElementById("three");
-  copthree.select();
-  document.execCommand("copy");
-}
-function copyfour(){
-  var copfour =document.getElementById("four");
-  copfour.select();
-  document.execCommand("copy");
-}
-function copyfive(){
-  var copfive =document.getElementById("five");
-  copfive.select();
-  document.execCommand("copy");
+function writeUserData( ref, price3,price6,price9,price12,price13,price18,price24,price30) {
+  firebase.database().ref(ref+'/').set({
+    price3: price3,
+    price6: price6,
+    price9:price9,
+    price12:price12,
+    price13:price13,
+    price18:price18,
+    price24:price24,
+    price30:price30,
+  });
 }
