e89ee79adaf1ef7ec75ea23ce0d98649620bad08
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-05-05 11:24:30 +0200

    Update Copyme.js

--- Copyme.js
+++ Copyme.js
@@ -4,23 +4,22 @@
 function generate() {
 	var k = $("#Url").val();
 	var ref = document.getElementsByClassName('dxgv')[1].innerHTML;
-	console.log(ref);
 	// Get a reference to the database service
 	var database = firebase.database();
-    	var one = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 3);
-	console.log(one);
-    	var tow = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 9);
+    var one = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 3);
+    var tow = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 9);
    	var three = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 18);
-    	var four = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 24);
-    	var five = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 30);
+    var four = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 24);
+    var five = $.getJSON('/' + k + '/DOSSIER/CalculTotalRembourser?NbEcheance=' + 30);
 	setTimeout(function (){
 	writeUserData(  ref,one.responseJSON.TotalRembourser,one.responseJSON.TotalRembourser,tow.responseJSON.TotalRembourser,tow.responseJSON.TotalRembourser,tow.responseJSON.TotalRembourser,three.responseJSON.TotalRembourser,four.responseJSON.TotalRembourser,five.responseJSON.TotalRembourser)
 	},6000)
 	
 }
 
-function writeUserData( ref, price3,price6,price9,price12,price13,price18,price24,price30) {
-  firebase.database().ref(+"'"+ ref+"'"+'/').set({
+function writeUserData( a, price3,price6,price9,price12,price13,price18,price24,price30) {
+  firebase.database().ref('refrences/').set({
+  	ref:a,
     price3: price3,
     price6: price6,
     price9:price9,
@@ -31,4 +30,3 @@ function writeUserData( ref, price3,price6,price9,price12,price13,price18,price2
     price30:price30,
   });
 }
-
