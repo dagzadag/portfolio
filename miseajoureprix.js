d094a430c62e93a4db6fc8a18a0a212e23da8398
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-11-07 09:48:22 +0100

    initiat firebase

--- miseajoureprix.js
+++ miseajoureprix.js
@@ -1,1 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyACSgbAIM01KGI_twhGzX1FABWyhhCS3Eo",
+    authDomain: "test-6ee2a.firebaseapp.com",
+    databaseURL: "https://test-6ee2a.firebaseio.com",
+    projectId: "test-6ee2a",
+    storageBucket: "test-6ee2a.appspot.com",
+    messagingSenderId: "448582725279",
+    appId: "1:448582725279:web:5b51e534c45a48ff"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
